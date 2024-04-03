48ff643cb025bb66f4db12bc69f28b528a8b8ff8
Micah Maphet <micahmaphet@gmail.com>
2024-04-03 12:55:41 -0400

    added favicon and handling for the chat and bot html pages

--- index.js
+++ index.js
@@ -6,12 +6,42 @@ const bot = require('./bot');
 const PORT = 8080;
 
 http.createServer((req, res) => {
-    fs.readFile('index.html', function(err, data) {
-        if (err) throw err;
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
+    var q = url.parse(req.url, true);
+    var page = q.pathname
+    console.log(page);
+    if (req.url == '/favicon.ico') {
+        fs.readFile('images/favicon.png', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+            res.end(data);
+        });
+        return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    if (page == "/") {
+        fs.readFile("index.html", (err, data) => {
+            if (err) throw err;
+            console.log("index.html");
+            res.write(data);
+            res.end()
+        });
+    } else if (page == "/chat") {
+        fs.readFile("chat.html", (err, data) => {
+            if (err) throw err;
+            console.log("chat.html");
+            res.write(data);
+        });
+    } else if (page == "/bot") {
+        fs.readFile("bot.html", (err, data) => {
+            if (err) throw err;
+            console.log("bot.html");
+            res.write(data);
+        });
+    } else res.end(`404 page ${page} could not be found`);
 }).listen(PORT);
 
 console.log(`Listening on port ${PORT}`);
