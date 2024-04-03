819935d76a1be062e118b3e8ee7f9ef7c44622f3
Micah Maphet <micahmaphet@gmail.com>
2024-04-03 09:22:44 -0400

    added html file to the front end

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 const bot = require('./bot');
 
 const PORT = 8080;
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(`Local Chat\n`);
-    var q = url.parse(req.url, true).query;
-    var txt = q.month + " " + q.day + "\n";
-    res.write(txt);
-    res.write(bot.phrases.Greating);
-    res.end();
+    fs.readFile('index.html', function(err, data) {
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 }).listen(PORT);
 
 console.log(`Listening on port ${PORT}`);
