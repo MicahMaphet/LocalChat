f8d5a3978f5fc3a20a2d00b4f39454b9004f764a
Micah Maphet <micahmaphet@gmail.com>
2024-04-02 22:31:37 -0400

    added url args for the month and the day for labeling the date on the page

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const http = require('http');
+const url = require('url');
 const bot = require('./bot');
 
 const PORT = 8080;
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(`Local Chat ${req.url}\n`);
+    res.write(`Local Chat\n`);
+    var q = url.parse(req.url, true).query;
+    var txt = q.month + " " + q.day + "\n";
+    res.write(txt);
     res.write(bot.phrases.Greating);
     res.end();
 }).listen(PORT);
