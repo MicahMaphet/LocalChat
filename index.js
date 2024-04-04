008d7aa30f268e034011c5fe0ff3c9d27da9abf5
Micah Maphet <micahmaphet@gmail.com>
2024-04-03 22:35:13 -0400

    added image file handling

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 http.createServer((req, res) => {
     var q = url.parse(req.url, true);
     var page = q.pathname
-    console.log(page);
+    console.log(req.url);
     if (req.url == '/favicon.ico') {
         fs.readFile('images/favicon.png', (err, data) => {
             if (err) {
@@ -21,24 +21,48 @@ http.createServer((req, res) => {
         });
         return;
     }
+    
+    if (req.url == '/images/bubble.png') {
+        fs.readFile('images/bubble.png', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+            res.end(data);
+        });
+        return;
+    }
+
+    if (req.url == '/images/bot.png') {
+        fs.readFile('images/bot.png', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+            res.end(data);
+        });
+        return;
+    }
+
     res.writeHead(200, {'Content-Type': 'text/html'});
-    if (page == "/") {
+    if (req.url == "/") {
         fs.readFile("index.html", (err, data) => {
             if (err) throw err;
-            console.log("index.html");
             res.write(data);
             res.end()
         });
-    } else if (page == "/chat") {
+    } else if (req.url == "/chat") {
         fs.readFile("chat.html", (err, data) => {
             if (err) throw err;
-            console.log("chat.html");
             res.write(data);
         });
-    } else if (page == "/bot") {
+    } else if (req.url == "/bot") {
         fs.readFile("bot.html", (err, data) => {
             if (err) throw err;
-            console.log("bot.html");
             res.write(data);
         });
     } else res.end(`404 page ${page} could not be found`);
